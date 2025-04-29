f08e8d2ac54457d23cb8f9b1dbce3c58375d1d86
Rohit <140701437+7206582513@users.noreply.github.com>
2025-04-29 12:05:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ UPLOAD_FOLDER = 'uploads'
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 
 # --- API Setup ---
-GROQ_API_KEY = "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"
+GROQ_API_KEY = "Your_Groq_API_KEY"
 GROQ_API_URL = "https://api.groq.com/openai/v1/chat/completions"
 GROQ_MODEL = "llama3-8b-8192"
 
